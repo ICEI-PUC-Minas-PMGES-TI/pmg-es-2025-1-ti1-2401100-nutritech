f8208e72bd2a24f0e07449835f93df8de2e061e5
larissa moncao <larissamoncaomoncao@gmail.com>
2025-05-03 21:29:05 -0300

    Alterações json e js

--- codigo/larissa_moncao/app.js
+++ codigo/larissa_moncao/app.js
@@ -1,4 +1,4 @@
-// Define o CPF do doador (modo de teste)
+// Define o CPF do doador (enquanto ainda não tenho um sistema de login)
 const CPF = '123.456.789-00';
 
 fetch('doacoes.json')
@@ -17,27 +17,33 @@ fetch('doacoes.json')
       lista.innerHTML = '<p>Nenhuma doação encontrada para este doador.</p>';
       info.innerHTML = '<p>Dados pessoais não encontrados.</p>';
     } else {
-      // Exibe os dados pessoais (assumindo que são os mesmos em todas as doações)
-      const dadosPessoais = doacoesFiltradas[0].doacao;
+        // Exibe os dados pessoais do doador e as doações
+        const dadosPessoais = doacoesFiltradas[0].doacao;
 
       info.innerHTML = `
         <div class="card-pessoal">
           <strong>Nome:</strong> ${dadosPessoais.nome}<br>
           <strong>Email:</strong> ${dadosPessoais.email}<br>
+          <strong>Cidade:</strong> ${dadosPessoais.cidade}<br>
+          <strong>Estado:</strong> ${dadosPessoais.estado}<br>
           <strong>Telefone:</strong> ${dadosPessoais.telefone}<br>
           <strong>CPF:</strong> ${dadosPessoais.CPF}
         </div>
       `;
 
-      // Exibe as doações
       doacoesFiltradas.forEach(item => {
         const div = document.createElement('div');
         div.classList.add('doacao');
         div.innerHTML = `
           <strong>Data:</strong> ${item.data}<br>
+          <strong>Instituição:</strong> ${item.instituicao}<br>
           <strong>Descrição:</strong> ${item.descricao}<br>
+          <strong>Categoria:</strong> ${item.categoria}<br>
           <strong>Quantidade:</strong> ${item.quantidade}<br>
           <strong>Valor:</strong> R$ ${parseFloat(item.valor).toFixed(2)}
+          <strong>entrega:</strong> ${item.entrega}<br>
+          <strong>status:</strong> ${item.status}<br>
+          <strong>Comprovante:</strong> <a href="${item.comprovante}" target="_blank">Ver Comprovante</a><br>
         `;
         lista.appendChild(div);
       });
