487dbbc9e528daf78be97235bb6abc0a9c31a905
Artur Neves <arturfneves17@gmail.com>
2025-05-04 12:14:44 -0300

    adicao de bootstrap, responsividade e alteracao do css e json

--- codigo/artur_de_figueiredo_neves/ranking.js
+++ codigo/artur_de_figueiredo_neves/ranking.js
@@ -3,9 +3,27 @@ fetch('rankingtemp.json')
   .then(data => {
     const lista = document.getElementById('ranking-list');
 
-    data.forEach(item => {
+    
+    const ranking = {};
+
+    data.doacoes_alimentos.forEach(doacao => {
+      const nome = doacao.doador.nome;
+      const valor = doacao.valor;
+
+      if (!ranking[nome]) {
+        ranking[nome] = 0;
+      }
+      ranking[nome] += valor;
+    });
+
+    
+    const rankingOrdenado = Object.entries(ranking)
+      .sort((a, b) => b[1] - a[1]);
+
+   
+    rankingOrdenado.forEach(([nome, valor], index) => {
       const li = document.createElement('li');
-      li.innerHTML = `<strong>${item.posicao}º</strong> ${item.nome} <span>R$ ${item.valor.toFixed(2).replace('.', ',')}</span>`;
+      li.innerHTML = `<strong>${index + 1}º</strong> ${nome} <span>R$ ${valor.toFixed(2).replace('.', ',')}</span>`;
       lista.appendChild(li);
     });
   })
