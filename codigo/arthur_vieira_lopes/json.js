fa4c29916180eeec8cf9ed87b2dbac69c254c89b
arthurvieiralopes <lucasbarrosfc556@gmail.com>
2025-05-05 13:00:34 -0300

    Estilização, criação do javascript e json

---
+++ codigo/arthur_vieira_lopes/json.js
@@ -0,0 +1,65 @@
+{
+    "site": {
+      "titulo": "Perfil da Ong",
+      "logo": {
+        "imagem": "imgs/logo.png",
+        "alt": "Logo",
+        "titulo": "Alimentação Para Todos"
+      },
+      "usuario": {
+        "nome": "Usuário",
+        "imagem": "imgs/usuario.png",
+        "link": "https://example.com"
+      },
+      "ong": {
+        "nome": "ONG Mãos que Alimentam",
+        "bio": "Somos a Mãos que Alimentam, uma ONG dedicada a combater a fome e promover a dignidade de famílias em situação de vulnerabilidade. Por meio de doações, parcerias e ações comunitárias, levamos alimentos e esperança a quem mais precisa. Acreditamos que ninguém deve enfrentar o dia com o estômago vazio — e trabalhamos todos os dias para transformar essa realidade.",
+        "data_entrada": "Março de 2023",
+        "localizacao": "Belo Horizonte - MG",
+        "familias_ajudadas": 12,
+        "redes_sociais": [
+          {
+            "nome": "Instagram",
+            "url": "https://instagram.com",
+            "icone": "fa-brands fa-instagram"
+          },
+          {
+            "nome": "Facebook",
+            "url": "https://facebook.com",
+            "icone": "fa-brands fa-facebook"
+          }
+        ]
+      },
+      "formulario": {
+        "id": "form-cartao",
+        "botao_doador": {
+          "texto": "Doar",
+          "id": "btn-submit"
+        }
+      },
+      "footer": {
+        "decoracoes": [
+          {
+            "imagem": "imgs/forma3.png",
+            "alt": "decoracao-rodape1",
+            "id": "forma3"
+          },
+          {
+            "imagem": "imgs/forma4.png",
+            "alt": "decoracao-rodape2",
+            "id": "forma4"
+          }
+        ],
+        "informacoes": {
+          "contato": {
+            "telefone": "(31) 9999-6666",
+            "email": "nutritech@pucminas.com.br"
+          },
+          "localizacao": "Belo Horizonte - MG"
+        }
+      }
+    }
+  }
+  
+  
+  
---
+++ codigo/arthur_vieira_lopes/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const btn = document.getElementById('btn-submit');
+  
+    btn.addEventListener('click', function () {
+      window.location.href = '../raphael_thierry_coelho/cartao.html'; // Ajuste esse caminho conforme sua estrutura de pastas
+    });
+  });
+  
