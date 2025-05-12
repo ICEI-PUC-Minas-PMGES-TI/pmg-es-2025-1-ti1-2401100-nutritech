b7c6f25600c25b00e8010bac59b65b20c172b7d8
arthurvieiralopes <lucasbarrosfc556@gmail.com>
2025-05-12 16:29:35 -0300

    Correção do footer e header, atualização do estilo.

--- codigo/arthur_vieira_lopes/script.js
+++ codigo/arthur_vieira_lopes/script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const btn = document.getElementById('btn-submit');
+    const btn = document.getElementById('btn-submit-2');
   
     btn.addEventListener('click', function () {
       window.location.href = '../raphael_thierry_coelho/cartao.html'; // Ajuste esse caminho conforme sua estrutura de pastas
